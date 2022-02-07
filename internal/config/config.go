de3c62796ec0274f6bb097293390baeaf85d2295
Adam Chalkley <atc0005@users.noreply.github.com>
2022-02-07 04:16:08 -0600

    Fix linting error
    refs GH-114

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,7 @@ import (
 
 // Version reflects the application version. This is overridden via Makefile
 // for release builds.
-var Version string = "dev build"
+var Version = "dev build"
 
 // MyAppName is the branded name of this application/project. This value will
 // be used in user-facing output.
